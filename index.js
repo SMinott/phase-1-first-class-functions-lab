c5a4693fd3f399bfa7cd9dbb1daf9fca1f581051
Susan Minott <susan_minott@yahoo.com>
2022-05-01 16:44:39 -0400

    commit tree a26c7898cf17934d338efb80b5f08861b5328007

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+// 1st code
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0,2)
+}
+
+// 2nd Code
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(2)
+//Could have used -2 as the index after slice
+}
+
+// 3rd Code
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+// 4th Code
+function createFareMultiplier(int){
+    return function(fare){
+        return int * fare
+    };
+};
+
+// 5th Code
+const fareDoubler = createFareMultiplier(2)
+
+// 6th Code
+const fareTripler = createFareMultiplier(3)
+
+//  7th Code
+function selectDifferentDrivers(drivers, eitherFunc){
+    return eitherFunc(drivers)
+}
